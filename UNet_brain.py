23d15917499d478585039f88b4218dbd46f0c2d8
FluffyL <48116565+FluffyL@users.noreply.github.com>
2019-04-21 23:45:39 -0400

    Update UNet_brain.py

--- UNet_brain.py
+++ UNet_brain.py
@@ -1,16 +1,14 @@
 import os
 import time
 import nibabel as nib
-import numpy as np
 
 import torch
-import torchvision
 from torch.utils.data import Dataset
 from torchvision import transforms
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 import torch.nn as nn
-import pandas as pd
+import matplotlib.pyplot as plt
 
 from predata_LPBA40 import load_dataset,prepare_validation,crop_data,convert_one_hot
 
@@ -18,9 +16,12 @@ from predata_LPBA40 import load_dataset,prepare_validation,crop_data,convert_one
 
 RANDOM_SEED = 123
 patch_size = 32
-dataset_dir = '/LPBA40/native_space/'
-croped_dir = '/LPBA40/train_data/'
+dataset_dir = r'D:\2019\lly\LPBA40\native_space'
+croped_dir = r'D:\2019\lly\LPBA40\train_data'
+predict_dir = r'D:\2019\lly\LPBA40\predict'
+
 s_train = 30#the num of subject used for training
+s_predict = 2#the num of subject need to be predict
 
 if torch.cuda.is_available():
     torch.backends.cudnn.deterministic = True
@@ -41,7 +42,7 @@ class MRIDataset(Dataset):
 		self.labels_transform = labels_transform
 
 	def __getitem__(self, index):
-
+		index = index + 1
 		[T1,label] = load_dataset(self.croped_dir, index, self.mode)
 		#if read .hdr files
 		T1 = T1[:,:,:,0]
@@ -57,8 +58,6 @@ class MRIDataset(Dataset):
 	def __len__(self):
 		return self.t_num
 
-
-
 img_transform = transforms.Compose([
 	#transforms.RandomAffine(degrees=(-20, 20), translate=(0.15, 0.15)),
 	transforms.ToTensor(), 
@@ -73,47 +72,49 @@ labels_transform = transforms.Compose([
 
 total_train = 0
 total_test = 0
+total_predict = 0
 
-for subject_id in range(1,s_train+1):
-	total_train = total_train + crop_data(dataset_dir, croped_dir, subject_id, total_train, mode=0, patch_size=32)
+#for subject_id in range(1,s_train+1):
+#	total_train = total_train + crop_data(dataset_dir, croped_dir, subject_id, total_train, mode=0, patch_size=32)
 
-for subject_id in range(s_train+1,41):
-	total_test = total_test + crop_data(dataset_dir, croped_dir, subject_id, total_test, mode=1, patch_size=32)
+#for subject_id in range(s_train+1,41):
+#	total_test = total_test + crop_data(dataset_dir, croped_dir, subject_id, total_test, mode=1, patch_size=32)
+
+#for subject_id in range(s_predict):
+#	total_predict = total_predict + crop_data(predict_dir, croped_dir, subject_id, total_test, mode=2, patch_size=32)
 
 BATCH_SIZE = 8
 train_dataset = MRIDataset(croped_dir,
-			t_num = total_train,
+			t_num = 15000,
 			mode = 0,
 			img_transform = img_transform,
 			labels_transform = img_transform)
 
 train_loader = DataLoader(dataset=train_dataset,
                           batch_size=BATCH_SIZE,
-                          shuffle=True,
-                          num_workers=4)
+                          shuffle=True)
+                          #num_workers=1)
 
 valid_dataset = MRIDataset(croped_dir,
-			t_num = total_test,
+			t_num = 5000,
 			mode = 1,
 			img_transform = img_transform,
 			labels_transform = img_transform)
 
 valid_loader = DataLoader(dataset=valid_dataset,
                           batch_size=BATCH_SIZE,
-                          shuffle=True,
-                          num_workers=4)
-'''
-predict_dataset = predictDataset(dataset_dir = dataset_dir,
-			num_s = 31,
-			num_e = 40,
-			img_transform = None,
-			labels_transform = None)
+                          shuffle=True)
+                          #num_workers=1)
 
-predict_loader = DataLoader(dataset=valid_dataset,
-                          batch_size=BATCH_SIZE,
-                          shuffle=True,
-                          num_workers=4)
-'''
+#predict_dataset = PredictDataset(dataset_dir = predict_dir,
+#			t_num = total_predict,
+#			img_transform = None)
+
+#predict_loader = DataLoader(dataset=predict_dataset,
+#                         batch_size=BATCH_SIZE,
+#                        shuffle=True,
+#                          num_workers=4)
+
 torch.manual_seed(0)
 
 num_epochs = 2
@@ -124,7 +125,8 @@ for epoch in range(num_epochs):
 		print('Epoch:', epoch+1, end='')
 		print(' | Batch index:', batch_idx, end='')
 		print(' | Batch size:', labels.size()[0])
-		
+		T1 = T1.type(torch.FloatTensor)
+		labels = labels.type(torch.FloatTensor)
 		T1 = T1.view(-1, 1,patch_size,patch_size,patch_size).to(DEVICE)
 		labels = labels.view(-1, 1,patch_size,patch_size,patch_size).to(DEVICE)
 		print('Image shape', T1.shape)
@@ -232,8 +234,9 @@ class UNet(nn.Module):
 		self.down1 = down(32, 64)
 		self.down2 = down(64, 128)
 		self.down3 = down(128, 256)
-		#self.down4 = down(256, 512)
-		self.center = UnetConvBlock(256,512,is_batchnorm = True)
+		self.down4 = down(256, 512)
+		self.center = UnetConvBlock(512,1024,is_batchnorm = True)
+		self.up5 = up(1024, 512)
 		self.up4 = up(512, 256)
 		self.up3 = up(256, 128)
 		self.up2 = up(128, 64)
@@ -245,7 +248,9 @@ class UNet(nn.Module):
 		x2 = self.down1(x1)
 		x3 = self.down2(x2)
 		x4 = self.down3(x3)
-		x5 = self.center(x4)
+		x5 = self.down4(x4)
+		x6 = self.center(x5)
+		x5 = self.up5(x6)
 		x4 = self.up4(x5)
 		x3 = self.up3(x4)
 		x2 = self.up2(x3)
@@ -267,70 +272,94 @@ torch.manual_seed(RANDOM_SEED)
 model = UNet(n_channels=1, n_classes=4)
 model = model.float()
 
-optimizer = torch.optim.SGD(model.parameters(), lr=0.5,momentum=0.9)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.05,momentum=0.9)
 
 #################################
 ### Training
 #################################
+def focal_loss(predict,label):
+	gamma = 2
+	alpha = 0.25
 
-def dice_IOU(predict, label):
-	#define loss function
-	alpha = 0.5
-	beta = 0.5
+	label = convert_one_hot(label)
 	
-	ones = np.ones(np.shape(label))
-	num = np.sum(p0*g0)
-	den = num + alpha * np.sum(p0*g1) + beta * np.sum(p1*g0)
-	T = np.sum(num/den)
-	label = label.view(predict.shape[-1])
-	return label-T
+	label_num = label.shape
+	ones = torch.ones(label_num)
+	zeros = torch.zeros(label_num)
+	
+	return cost
+
+
+def tversky(predict, label):
+	#define loss function
+	'''
+	Dice : alpha=beta=0.5
+	jaccard : alpha=beta=1
+	variant Dice : alpha+beta=1
+	'''
+	alpha = 1
+	beta = 1
+
+	#convert labels to one_hot matrix
+	label = convert_one_hot(label)
+
+	label_num = label.shape
+	ones = torch.ones(label_num)
+	p0 = predict
+	p1 = ones - p0
+	g0 = label
+	g1 = ones - g0
+
+	TP = torch.sum(p0 * g0)
+	FP = torch.sum(p1 * g0)
+	FN = torch.sum(p0 * g1)
+	tversky = TP / (TP + alpha*FN + beta*FP)
+	cost = 1-tversky
+
+	return cost
 
 def Dice_3D(predict,label):
 	label = convert_one_hot(label)
+
 	label_num = label.shape
-	predict = predict.detach().numpy()
-	cost = 0 
-	for i in range(label_num[1]):
-		p0 = np.zeros(np.shape(label[:,i,:,:,:]))
-		p0 = label[:,i,:,:,:]
-		ones = np.ones(np.shape(p0))
-		g0 = predict[:,0,:,:,:]
+	ones = torch.ones(label_num)
+	p0 = predict
+	p1 = ones - p0
+	g0 = label
+	g1 = ones - g0
 
-		p_true = p0*g0
-		p_true[p_true < i] = 0
+	TP = torch.sum(p0 * g0)
+	FP = torch.sum(p1 * g0)
+	FN = torch.sum(p0 * g1) 
 
-		p_true_ones = np.zeros(np.shape(p_true))
-		p_true_ones[p_true > 0] = 1
-		
-		p_predict = np.zeros(np.shape(p_true))
-		p_predict[p0 - i > 0] = 1
-		p_predict[p0 - i > 1] = 0
-
-		dice = 2*np.sum(p_true)/(np.sum(g0) + np.sum(p_predict))
-		cost = cost + dice
-	return (-1)*cost
+	dice = 2*TP / (2*TP + FP + FN)
+	cost = 1-dice
+	return cost
 
 def focal_loss(predict, label):
 	gamma = 2
 	alpha = 0.25
 
+	pt_1 = torch.where(torch.equal(label,1), predict,torch.ones(predict))
+	pt_0 = torch.where(torch.equal(label,0), predict,torch.ones(predict))
+	#cost = (-1)*torch.sum(alpha*torch.pow(1. - pt_1, gamma) * torch.log(pt_1))-torch.sum((1-alpha
 	return 0
 						
 
 def compute_epoch_loss(model, data_loader):
 	curr_loss, num_examples = 0., 0
-	loss_func = dice()
 	with torch.no_grad():
 		for T1, labels in data_loader:
 			T1 = T1.view(-1,1,patch_size,patch_size,patch_size).to(DEVICE)
-			labels = labels.view(-1,1,patch_size,patch_size,patch_size).to(DEVICE)
+			labels = labels.view(-1,patch_size,patch_size,patch_size).to(DEVICE)
+			T1 = T1.type(torch.FloatTensor)
+			labels = labels.type(torch.FloatTensor)
 			logits, probas = model(T1)
-			#labels = convert_one_hot(probas)
 			#loss = 0
-			#loss = Dice_3D(probas,labels)/BATCH_SIZE
-			cost = F.cross_entropy(logits, labels)
+			loss = Dice_3D(probas,labels)
+			#cost = F.cross_entropy(logits, labels)
 			
-			num_examples += labels.size(0)
+			num_examples += labels.size(0)*32*32*32
 			curr_loss += loss
 
 		curr_loss = curr_loss / num_examples
@@ -344,11 +373,14 @@ def compute_accuracy(model, data_loader):
 	correct_pred, num_examples = 0, 0
 	with torch.no_grad():
 		for T1, labels in data_loader:
+
 			T1 = T1.view(-1,1,patch_size,patch_size,patch_size).to(DEVICE)
-			labels = labels.view(-1,1,patch_size,patch_size,patch_size).to(DEVICE)
+			labels = labels.view(-1,patch_size,patch_size,patch_size).to(DEVICE)
+			T1 = T1.type(torch.FloatTensor)
+			labels = labels.type(torch.FloatTensor)
 			logits, probas = model.forward(T1)
 			predicted_labels = torch.argmax(probas, 1)
-			num_examples += labels.size(0)
+			num_examples += labels.size(0)*32*32*32
 			correct_pred += (predicted_labels == labels).sum()
 		return correct_pred.float()/num_examples * 100
 
@@ -363,15 +395,16 @@ for epoch in range(NUM_EPOCHS):
 	model.train()
 	cost = 0
 	for batch_idx, (T1, labels) in enumerate(train_loader):
+
 		T1 = T1.view(-1,1,patch_size,patch_size,patch_size).to(DEVICE)
 		#labels = labels.view(-1,patch_size,patch_size,patch_size).to(DEVICE)
 		labels = labels.long().to(DEVICE)
-            
+		T1 = T1.type(torch.FloatTensor)
+		labels = labels.type(torch.FloatTensor)
 		### FORWARD AND BACK PROP
 		logits, probas = model(T1)
-		
-		#cost = Dice_3D(probas, labels)/BATCH_SIZE
-		cost = F.cross_entropy(logits, labels)
+		cost = Dice_3D(probas, labels)
+		#cost = F.cross_entropy(logits, labels)
 		optimizer.zero_grad()
         
 		cost.backward()
@@ -384,12 +417,13 @@ for epoch in range(NUM_EPOCHS):
 			print ('Epoch: %03d/%03d | Batch %03d/%03d | Cost: %.4f' 
  				%(epoch+1, NUM_EPOCHS, batch_idx, 
 				len(train_loader), cost))
+			torch.save(model, os.path.join(predict_dir ,'model.pt'))
         
 	model.eval()
     
 	cost = compute_epoch_loss(model, train_loader)
 	epoch_cost.append(cost)
-    
+	torch.save(model,os.path.join(predict_dir,'model.pt'))
 	train_accuracy = compute_accuracy(model, train_loader)
 	valid_accuracy = compute_accuracy(model, valid_loader)
     
@@ -400,18 +434,15 @@ for epoch in range(NUM_EPOCHS):
     
 print('Total Training Time: %.2f min' % ((time.time() - start_time)/60))
 
-'''
 
 plt.plot(range(len(minibatch_cost)), minibatch_cost)
-plt.ylabel('Cross Entropy')
+plt.ylabel('cost')
 plt.xlabel('Minibatch')
 plt.show()
 
 plt.plot(range(len(epoch_cost)), epoch_cost)
-plt.ylabel('Cross Entropy')
+plt.ylabel('cost')
 plt.xlabel('Epoch')
 plt.show()
 
-
-print('Test Accuracy: %.2f' % compute_accuracy(model, test_loader))
-'''
+print('Test Accuracy: %.2f' % compute_accuracy(model, valid_loader))
